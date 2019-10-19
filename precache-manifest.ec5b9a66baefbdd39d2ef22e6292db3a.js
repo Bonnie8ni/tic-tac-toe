8873580d26d19e5b19d6cb6b9244ede9c97b5e5f
BonnieYu <Bonnie_Yu@wistron.com>
2019-10-20 03:34:43 +0800

    Updates

---
+++ precache-manifest.ec5b9a66baefbdd39d2ef22e6292db3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f0340678499e0194fd0d9c804b7159",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f7427075bb45c6089696",
+    "url": "/tic-tac-toe/static/css/main.49d247e9.chunk.css"
+  },
+  {
+    "revision": "83570a2a58db1debbe6a",
+    "url": "/tic-tac-toe/static/js/2.d17b2bdb.chunk.js"
+  },
+  {
+    "revision": "f7427075bb45c6089696",
+    "url": "/tic-tac-toe/static/js/main.ab81a78a.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
